705528b5ba1119f345bfc4120625054d7fad35c1
bhaskar217 <mbrkashyap@gmail.com>
2026-06-06 01:03:39 +0530

    Update data_register.py

--- week_2_mls/model_building/data_register.py
+++ week_2_mls/model_building/data_register.py
@@ -1,22 +1,20 @@
-from huggingface_hub.utils import RepositoryNotFoundError, HfHubHTTPError
+from huggingface_hub.utils import RepositoryNotFoundError
 from huggingface_hub import HfApi, create_repo
 import os
 
-
+token = os.getenv("HF_TOKEN")
 repo_id = "bhaskar217/machine-failure-prediction"
 repo_type = "dataset"
 
-# Initialize API client
-api = HfApi(token=os.getenv("HF_TOKEN"))
+api = HfApi(token=token)
 
-# Step 1: Check if the space exists
 try:
     api.repo_info(repo_id=repo_id, repo_type=repo_type)
-    print(f"Space '{repo_id}' already exists. Using it.")
+    print(f"Dataset '{repo_id}' already exists. Using it.")
 except RepositoryNotFoundError:
-    print(f"Space '{repo_id}' not found. Creating new space...")
-    create_repo(repo_id=repo_id, repo_type=repo_type, private=False)
-    print(f"Space '{repo_id}' created.")
+    print(f"Dataset '{repo_id}' not found. Creating...")
+    create_repo(repo_id=repo_id, repo_type=repo_type, private=False, token=token)
+    print(f"Dataset '{repo_id}' created.")
 
 api.upload_folder(
     folder_path="week_2_mls/data",
