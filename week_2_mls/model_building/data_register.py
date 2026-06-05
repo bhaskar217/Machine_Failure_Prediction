792bed44fad9c96e100e998336924931f74d7df7
bhaskar217 <mbrkashyap@gmail.com>
2026-06-05 18:41:41 +0000

    first commit

---
+++ week_2_mls/model_building/data_register.py
@@ -0,0 +1,25 @@
+from huggingface_hub.utils import RepositoryNotFoundError, HfHubHTTPError
+from huggingface_hub import HfApi, create_repo
+import os
+
+
+repo_id = "bhaskar217/machine-failure-prediction"
+repo_type = "dataset"
+
+# Initialize API client
+api = HfApi(token=os.getenv("HF_TOKEN"))
+
+# Step 1: Check if the space exists
+try:
+    api.repo_info(repo_id=repo_id, repo_type=repo_type)
+    print(f"Space '{repo_id}' already exists. Using it.")
+except RepositoryNotFoundError:
+    print(f"Space '{repo_id}' not found. Creating new space...")
+    create_repo(repo_id=repo_id, repo_type=repo_type, private=False)
+    print(f"Space '{repo_id}' created.")
+
+api.upload_folder(
+    folder_path="/content/drive/My Drive/Colab Notebooks/Course 10 Advanced AI & ML Ops/Week 2/week_2_mls/data/",
+    repo_id=repo_id,
+    repo_type=repo_type,
+)
