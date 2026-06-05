b6a736b92b61f6c36f9490edf14817041cc65e81
bhaskar217 <mbrkashyap@gmail.com>
2026-06-06 00:57:37 +0530

    Update hosting.py

--- week_2_mls/hosting/hosting.py
+++ week_2_mls/hosting/hosting.py
@@ -3,7 +3,7 @@ import os
 
 api = HfApi(token=os.getenv("HF_TOKEN"))
 api.upload_folder(
-    folder_path="/content/drive/My Drive/Colab Notebooks/Course 10 Advanced AI & ML Ops/Week 2/week_2_mls/deployment",     # the local folder containing your files
+    folder_path="/week_2_mls/deployment",     # the local folder containing your files
     repo_id="bhaskar217/Machine-Failure-Prediction",          # the target repo
     repo_type="space",                      # dataset, model, or space
     path_in_repo="",                          # optional: subfolder path inside the repo
